4614bdac2f14015646de7c8fa24865e56bdac3c7
Minjae Seon <ddol0225@gmail.com>
2021-08-27 15:03:15 +0900

    Initial Commit

---
+++ app/src/main/java/kr/ac/gachon/sw/safenoisecanceling/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.gachon.sw.safenoisecanceling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.common.GoogleApiAvailability
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
